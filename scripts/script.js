71f13838f8d34ae8d0b7e07463ee278f9a258d7d
ViniciusLisboa07 <63267152+ViniciusLisboa07@users.noreply.github.com>
2020-08-07 18:54:13 -0300

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,9 @@ btnLogin.addEventListener("click", event => {
     event.preventDefault();
     container.classList.add("container-hide");
 });
+
+container.addEventListener("animationend", (event) => {
+    if(event.animationName == "down"){
+        container.style.display = "none";
+    }
+});
