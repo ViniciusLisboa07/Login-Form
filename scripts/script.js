7e2e0565eac722e68acb922bb078c327a4f0db64
ViniciusLisboa07 <63267152+ViniciusLisboa07@users.noreply.github.com>
2020-08-08 15:57:15 -0300

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,25 @@ const container = document.querySelector(".container");
 
 btnLogin.addEventListener("click", event => {
     event.preventDefault();
+
+    const fields = [ ...document.querySelectorAll(".input-block input")];
+
+    fields.forEach(filed => {
+        if(fild.value == "") container.classList.add("validate-error")
+    });
+
+    const formError = document.querySelector(".validate-error");    
+
     container.classList.add("container-hide");
+    if(formError){
+        formError.addEventListener("animationend", (event) => {
+            if(event.animationName == "nono"){
+                formError.classList.remove("validate-error");
+            }
+        });
+    } else{
+        formError.classList.add("form-hide");
+    }
 });
 
 container.addEventListener("animationstart", event => {
